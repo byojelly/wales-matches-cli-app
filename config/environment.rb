a3cfca7f0de123f24e45489ed1013fe556240062
Alex <github email address>
2017-08-01 15:44:11 -0400

    setting up work environment

---
+++ config/environment.rb
@@ -0,0 +1,21 @@
+#lets give our project access to gems
+
+require 'bundler'
+Bundler.require
+
+
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+# the below require_all was grabbed from the Group project repo project
+require_all 'lib'
+
+
+
+
+#below  are from template project, update once my lib is more refined
+#require_relative '../lib/scraper'
+#require_relative '../lib/worlds_best_restaurants/restaurant'
+#require_relative '../lib/worlds_best_restaurants/cli'
