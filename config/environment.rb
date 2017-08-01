6bf87bdfdaed3772e7f6ead03d1cb5b065e9d99a
Alex <github email address>
2017-08-01 15:46:57 -0400

    work in progress

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,6 @@ require_all 'lib'
 
 
 #below  are from template project, update once my lib is more refined
-#require_relative '../lib/scraper'
+#require_relative '../lib/scraper.rb'
 #require_relative '../lib/worlds_best_restaurants/restaurant'
 #require_relative '../lib/worlds_best_restaurants/cli'
