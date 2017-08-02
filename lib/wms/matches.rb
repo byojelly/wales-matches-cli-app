66e4e7bb18320062325a34bbca0b22d5786c00bf
Alex <github email address>
2017-08-02 17:40:45 -0400

    adding matched.rb file to create the matches class

---
+++ lib/wms/matches.rb
@@ -0,0 +1,5 @@
+class WalesMatchSchedule::Match
+
+  attr_accessor :date, :time, :comp, :home, :score, :away, :venue, :more_info
+
+end
---
+++ lib/wms/scraper.rb
@@ -0,0 +1,6 @@
+#lets access the module from the previous folder
+require_relative '../wms.rb'
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
